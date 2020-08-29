7b7977af722d5f09a8955b581390eb5aabec0815
tremael Arrington <timewizard2500@gmail.com>
2020-08-29 18:28:35 -0400

    Started user database

--- Schedule Manager/schedule_manager/schedule/models.py
+++ Schedule Manager/schedule_manager/schedule/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# record each task
+class Task(models.Model):
+    # deadline
+    deadline = models.DateField()
+
+    # expected time to completion (in hours)
+    time2complete = models.IntegerField()
+
+
+# record the user's unavailable time
+class UnavailableTime(models.Models):
+    # time that the user is asleep or otherwise incapable/unwilling to be productive
+    unavailableTimeStart = models.IntegerField()
+    unavailableTimeEnd = models.IntegerField()
